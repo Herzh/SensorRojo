d073aadbae9f063077d7429d75272a2d02278446
herzh <alucin_nm@hotmail.com>
2020-04-24 22:56:58 -0500

    ya sensa el valor y se retiene, falta el contenedor rojo

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,13 @@ class MyApp extends StatefulWidget {
 
 class Estado extends State{
   AccelerometerEvent acelerometro; //Acelerómetro con Gravedad
-  //UserAccelerometerEvent acelerometro; //Acelerómetro sin Gravedad
-  //GyroscopeEvent giroscopio;
 
   @override
   void initState(){
     super.initState();
     accelerometerEvents.listen((AccelerometerEvent event) {
-    //userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-    //gyroscopeEvents.listen((GyroscopeEvent event) {
       setState(() {
         acelerometro = event;
-        //giroscopio = event;
       });
     });
   }
@@ -31,36 +26,12 @@ class Estado extends State{
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            /*
-              'Acelerómetro'
-              ' X: ${acelerometro.x.roundToDouble()}'
-              ' Y: ${acelerometro.y.roundToDouble()}'
-              ' Z: ${acelerometro.z.roundToDouble()}'
-            */
-            /*
-              'Giroscopio'
-              ' X: ${giroscopio.x.roundToDouble()}'
-              ' Y: ${giroscopio.y.roundToDouble()}'
-              ' Z: ${giroscopio.z.roundToDouble()}'
-             */
-
-              // Esta si guada valores:
-              //    Rot izq 0 -> 10 y 10 a 0
-              //    Rot der 0 -> -9 y -9 a 0
-              'Sensor Rojo (a.a. c/G)'
+          /* Esta si guada valores:
+             Rot izq 0 -> 10 y 10 a 0
+             Rot der 0 -> -9 y -9 a 0
+           */
+          title: Text('Sensor Rojo (a.a. c/G)'
                 ' X: ${acelerometro.x.roundToDouble()}'
-
-
-            /* Descartado: Solo mide cuando se mueve, pero con conserva el valor
-              'Sensor Rojo (a.a. s/G)'
-                ' X: ${acelerometro.x.roundToDouble()}'
-             */
-
-            /* Descartado: Solo mide cuando se mueve, pero con conserva el valor
-              'Sensor Rojo (Giroscopio)'
-                ' X: ${giroscopio.x.roundToDouble()}'
-            */
 
           ),
         ),
