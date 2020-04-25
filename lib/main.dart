6415e310c4c9a45ebc6d2dafbb8c86b29e3442f6
herzh <alucin_nm@hotmail.com>
2020-04-24 23:19:48 -0500

    se añade contenedor Rojo, pero aun no cambia la transparencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
+import 'dart:math';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() => Estado();
+
 }
 
-class Estado extends State{
+class Estado extends State<MyApp> {
   AccelerometerEvent acelerometro; //Acelerómetro con Gravedad
+
+  // valores por defecto.
+  double _width = 150;
+  double _height = 150;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   void initState(){
@@ -17,6 +27,7 @@ class Estado extends State{
     accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
         acelerometro = event;
+
       });
     });
   }
@@ -31,13 +42,26 @@ class Estado extends State{
              Rot der 0 -> -9 y -9 a 0
            */
           title: Text('Sensor Rojo (a.a. c/G)'
-                ' X: ${acelerometro.x.roundToDouble()}'
-
+              ' X: ${acelerometro.x.roundToDouble()}'
+          ),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            // Usa setState para reconstruir el widget con nuevos valores.
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            // Define la duración de la animación.
+            duration: Duration(seconds: 1),
+            // Proporciona una curva opcional para hacer que la animación se sienta más suave.
+            curve: Curves.fastOutSlowIn,
           ),
         ),
       ),
     );
   }
 }
-
 
