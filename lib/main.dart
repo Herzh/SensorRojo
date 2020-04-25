d395c79d8a4f177e93805905ead400350a4f6124
herzh <alucin_nm@hotmail.com>
2020-04-24 22:28:28 -0500

    se identifico el Acelerometro con Gravedad como el unico que conserva los valores, y se usara para la tarea Sensor Rojo

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,19 @@ class MyApp extends StatefulWidget {
 }
 
 class Estado extends State{
-  //AccelerometerEvent acelerometro; //Acelerómetro con Gravedad
+  AccelerometerEvent acelerometro; //Acelerómetro con Gravedad
   //UserAccelerometerEvent acelerometro; //Acelerómetro sin Gravedad
-  GyroscopeEvent giroscopio;
+  //GyroscopeEvent giroscopio;
 
   @override
   void initState(){
     super.initState();
-    //accelerometerEvents.listen((AccelerometerEvent event) {
+    accelerometerEvents.listen((AccelerometerEvent event) {
     //userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-    gyroscopeEvents.listen((GyroscopeEvent event) {
+    //gyroscopeEvents.listen((GyroscopeEvent event) {
       setState(() {
-        //acelerometro = event;
-        giroscopio = event;
+        acelerometro = event;
+        //giroscopio = event;
       });
     });
   }
@@ -38,10 +38,30 @@ class Estado extends State{
               ' Y: ${acelerometro.y.roundToDouble()}'
               ' Z: ${acelerometro.z.roundToDouble()}'
             */
+            /*
               'Giroscopio'
               ' X: ${giroscopio.x.roundToDouble()}'
               ' Y: ${giroscopio.y.roundToDouble()}'
               ' Z: ${giroscopio.z.roundToDouble()}'
+             */
+
+              // Esta si guada valores:
+              //    Rot izq 0 -> 10 y 10 a 0
+              //    Rot der 0 -> -9 y -9 a 0
+              'Sensor Rojo (a.a. c/G)'
+                ' X: ${acelerometro.x.roundToDouble()}'
+
+
+            /* Descartado: Solo mide cuando se mueve, pero con conserva el valor
+              'Sensor Rojo (a.a. s/G)'
+                ' X: ${acelerometro.x.roundToDouble()}'
+             */
+
+            /* Descartado: Solo mide cuando se mueve, pero con conserva el valor
+              'Sensor Rojo (Giroscopio)'
+                ' X: ${giroscopio.x.roundToDouble()}'
+            */
+
           ),
         ),
       ),
